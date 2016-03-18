de16c2056d33d0299e70226c346726cf6424001f
Tom Mason <wheybags@wheybags.com>
2016-03-18 22:33:25 +0000

    fix debian packages install + add a few packages

--- packages.sh
+++ packages.sh
@@ -2,7 +2,7 @@ PACKAGES="moreutils"
 
 OSX_PACKAGES="pyenv-virtualenv"
 
-LINUX_PACKAGES="python-virtualenv"
+LINUX_PACKAGES="python-virtualenv vim sl sm dconf-cli git-gui"
 
 unamestr=`uname`
 
@@ -10,6 +10,6 @@ if [ $unamestr == 'Darwin' ]; then
     echo "$PACKAGES $OSX_PACKAGES" | xargs brew install
 else
     if sudo true; then
-        echo "$PACKAGES $OSX_PACKAGES" | xargs sudo apt-get install
+        echo "$PACKAGES $LINUX_PACKAGES" | xargs sudo apt-get install -yy
     fi
 fi
